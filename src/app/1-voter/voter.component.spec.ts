727df851340d7be383b15421f547c7fd202449da
Eric Chang <efrochang@gmail.com>
2018-06-03 20:49:23 -0500

    TestBed and ComponentFixture

--- src/app/1-voter/voter.component.spec.ts
+++ src/app/1-voter/voter.component.spec.ts
@@ -1,8 +1,22 @@
+import { TestBed } from '@angular/core/testing';
+import { ComponentFixture } from '@angular/core/testing';
+
 import { VoterComponent } from './voter.component';
 
 describe('VoterComponent', () => {
 
   beforeEach(() => {
+
+    let fixture: ComponentFixture<VoterComponent>;
+    let component: VoterComponent;
+
+    TestBed.configureTestingModule({
+
+      declarations: [ VoterComponent ]
+    });
+
+    fixture = TestBed.createComponent( VoterComponent );
+    component = fixture.componentInstance;
   });
 
   it('', () => {
