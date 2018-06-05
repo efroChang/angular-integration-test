360c124c67795914e70f56bd911d8b802ffd8a32
Eric Chang <efrochang@gmail.com>
2018-06-04 21:22:18 -0500

    Mock Router and ActivatedRoute

--- src/app/3-user-details/user-details.component.spec.ts
+++ src/app/3-user-details/user-details.component.spec.ts
@@ -2,16 +2,35 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 import { By } from '@angular/platform-browser';
 import { DebugElement } from '@angular/core';
+import { Router, ActivatedRoute } from '@angular/router'; 
+import { Observable } from 'rxjs';
 
 import { UserDetailsComponent } from './user-details.component';
 
-xdescribe('UserDetailsComponent', () => {
+class RouterStub                  // [KEY]: The fake Router            
+{
+  navigate( params )
+  {
+  }
+}
+
+class ActivatedRouteStub          // [KEY]: The fake ActivatedRoute
+{
+  params: Observable<any> = Observable.empty();   // Returns an Observable
+}
+
+describe('UserDetailsComponent', () => {
+
   let component: UserDetailsComponent;
   let fixture: ComponentFixture<UserDetailsComponent>;
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ UserDetailsComponent ]
+      declarations: [ UserDetailsComponent ],
+      providers: [
+        { provide: Router, useClass: RouterStub },                        // [KEY]: Mock the Router provider
+        { provide: ActivatedRoute, useClass: ActivatedRouteStub }         // [KEY]: Mock the Router provider
+      ]
     })
     .compileComponents();
   }));
